e4fc258362bb584f993eed0a2994b713880d2400
allibis <andrix1250@gmail.com>
2025-09-19 12:06:50 +0200

    aggiunto la tazzina e spostato i file delle funzioni in una sottocartella

--- teiera.m
+++ teiera.m
@@ -201,7 +201,45 @@ S_piatto(:,:,1) = 3+S_piatto(:,:,1);
 S_piatto(:,:,2) = S_piatto(:,:,2) - 3;
 
 
+N = 50; % N = numero di punti (es. 200)
+a = 0.1; % a = ampiezza delle ondulazioni
 
+theta = linspace(0, 2*pi, N+1)';  % angolo [0,2pi]                  % rimuovo duplicato finale
+NP = 20;
+A = linspace(0,pi/2,NP);
+
+
+z = sin(A);
+y = zeros(size(z,2),1);
+x = 0.5./(1-linspace(0,.5,NP));
+
+plot3(x,y,z, 'r-o')
+axis equal
+xlabel('x'); ylabel('y'); zlabel('z');
+
+% creo il vettore S per rimuovere i warning
+S_tazzina = zeros(size(theta,1), NP, 3);
+for j=1:NP
+    R = x(j);
+
+    % descrivo la curva desiderata attraverso i parametri r, theta
+    r = R * (1 + a*cos(6*theta));    
+    
+    % applico un cambiamento di coordinate da polari a cartesiane
+    x_p = r .* cos(theta);
+    y_p = r .* sin(theta);
+    z_p = ones(size(x_p))*z(j);
+    
+    hold on
+    plot3(x_p, y_p, z_p);
+    S_tazzina(:,j,1) = x_p;   % coordinate x
+    S_tazzina(:,j,2) = y_p;   % coordinate y
+    S_tazzina(:,j,3) = z_p;   % coordinate z
+end
+
+S_tazzina(:,:,1) = 3+S_tazzina(:,:,1);
+S_tazzina(:,:,2) = S_tazzina(:,:,2) - 3;
+S_tazzina(:,:,3) = S_tazzina(:,:,3) + 1.3;
 
 
 %% visualizzazione
@@ -227,6 +265,9 @@ surf(T(:,:,1), T(:,:,2), T(:,:,3), ...
     'FaceColor', porcellana, 'EdgeColor', 'none', ...
     'FaceLighting', 'gouraud', 'SpecularStrength', 0.2);
 surf(S_piatto(:,:,1), S_piatto(:,:,2), S_piatto(:,:,3), ...
+    'FaceColor', porcellana, 'EdgeColor', 'none', ...
+    'FaceLighting', 'gouraud', 'SpecularStrength', 0.2);
+surf(S_tazzina(:,:,1), S_tazzina(:,:,2), S_tazzina(:,:,3), ...
     'FaceColor', porcellana, 'EdgeColor', 'none', ...
     'FaceLighting', 'gouraud', 'SpecularStrength', 0.2);
 
